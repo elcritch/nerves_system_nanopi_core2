97bc4bf2799c16b1c6d94f0ffde4e2eedf4fe97a
Jaremy Creechley <creechley@gmail.com>
2019-07-03 23:34:59 -0600

    updates

--- uboot/uboot-script.cmd
+++ uboot/uboot-script.cmd
@@ -89,7 +89,6 @@ booti $kernel_addr_r - $fdt_addr_r
 
 load mmc 0:1 ${kernel_addr_r} zImage
 
-#fdtfile=sun8i-h2-plus-orangepi-zero.dtb
 fdtfile=sun50i-h5-nanopi-neo2.dtb
 # Load the DT. On the BBB, fdtfile=sun8i-h3-nanopi-neo.dtb
 load mmc 0:1 ${fdt_addr_r} ${fdtfile}
