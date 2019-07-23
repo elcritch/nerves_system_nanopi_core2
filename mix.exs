8108e87e2fd6a949f60723c67001337395c10b19
Jaremy Creechley <creechley@gmail.com>
2019-07-22 19:35:51 -0600

    change toolchain to match platform

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule NervesSystemNanopiCore2.MixProject do
     [
       {:nerves, "~> 1.5", runtime: false},
       {:nerves_system_br, "1.7.2", runtime: false},
-      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "1.1.0", runtime: false},
+      # {:nerves_toolchain_arm_unknown_linux_gnueabihf, "1.1.0", runtime: false},
+      {:nerves_toolchain_aarch64_unknown_linux_gnueabi, "1.2.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
     ]
